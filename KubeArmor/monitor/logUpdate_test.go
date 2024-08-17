f8fd9e176bc044101510dd3db01367254cfeb767
tesla59 <nishant@heim.id>
2024-08-17 22:30:56 +0000

    Fuzz: add another Fuzzer for monitor package
    Signed-off-by: tesla59 <nishant@heim.id>

---
+++ KubeArmor/monitor/logUpdate_test.go
@@ -0,0 +1,75 @@
+package monitor
+
+import (
+	"fmt"
+	kl "github.com/kubearmor/KubeArmor/KubeArmor/common"
+	cfg "github.com/kubearmor/KubeArmor/KubeArmor/config"
+	"github.com/kubearmor/KubeArmor/KubeArmor/feeder"
+	tp "github.com/kubearmor/KubeArmor/KubeArmor/types"
+	"strings"
+	"sync"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	// containers
+	Containers := map[string]tp.Container{}
+	ContainersLock := new(sync.RWMutex)
+
+	// pid map
+	ActiveHostPidMap := map[string]tp.PidMap{}
+	ActivePidMapLock := new(sync.RWMutex)
+
+	// node
+	node := tp.Node{}
+	nodeLock := new(sync.RWMutex)
+
+	node.KernelVersion = kl.GetCommandOutputWithoutErr("uname", []string{"-r"})
+	node.KernelVersion = strings.TrimSuffix(node.KernelVersion, "\n")
+
+	_ = Containers
+	_ = ContainersLock
+	_ = ActiveHostPidMap
+	_ = ActivePidMapLock
+
+	// load configuration
+	if err := cfg.LoadConfig(); err != nil {
+		fmt.Println("[FAIL] Failed to load configuration")
+		return
+	}
+
+	// configuration
+	cfg.GlobalCfg.Policy = true
+	cfg.GlobalCfg.HostPolicy = true
+
+	// create logger
+	logger := feeder.NewFeeder(&node, &nodeLock)
+	if logger == nil {
+		fmt.Println("[FAIL] Failed to create logger")
+		return
+	}
+	fmt.Println("[PASS] Created logger")
+
+	// montor lock
+	monitorLock := new(sync.RWMutex)
+
+	// Create System Monitor
+	systemMonitor := NewSystemMonitor(&node, &nodeLock, logger, &Containers, &ContainersLock, &ActiveHostPidMap, &ActivePidMapLock, &monitorLock)
+	if systemMonitor == nil {
+		fmt.Println("[FAIL] Failed to create SystemMonitor")
+
+		if err := logger.DestroyFeeder(); err != nil {
+			fmt.Println("[FAIL] Failed to destroy logger")
+			return
+		}
+		return
+	}
+	fmt.Println("[PASS] Created SystemMonitor")
+	m.Run()
+}
+
+func FuzzUpdateLogs(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		_ = data
+	})
+}
--- oss-fuzz/build.sh
+++ oss-fuzz/build.sh
@@ -7,3 +7,4 @@ go get github.com/kubearmor/KubeArmor/KubeArmor/feeder
 
 compile_native_go_fuzzer github.com/kubearmor/KubeArmor/KubeArmor/feeder FuzzU fuzz_u
 compile_native_go_fuzzer github.com/kubearmor/KubeArmor/KubeArmor/feeder FuzzFeeder_PushLog fuzz_feeder_push_log
+compile_native_go_fuzzer github.com/kubearmor/KubeArmor/KubeArmor/monitor FuzzUpdateLogs fuzz_update_logs
